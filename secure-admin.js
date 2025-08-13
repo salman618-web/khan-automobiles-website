a4c042aef947785ce20443e1dad5822b5c2d7b83
Khan Automobiles <mrkhan618@gmail.com>
2025-08-13 11:49:19 +0530

    chore(deploy): no-op edits to retrigger Render (secure-admin.js, admin.html)

--- secure-admin.js
+++ secure-admin.js
@@ -1,6 +1,6 @@
 // Clean Secure Admin JavaScript - Works with simple server
 // No JWT tokens, no complex authentication - just working functionality
-// deploy: no-op 2
+// deploy: no-op 3
 
 // Global variables
 let currentUser = null;
