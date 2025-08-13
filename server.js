8e66213abeceb9aa994aa272ca38e3a5bbb604a7
Khan Automobiles <mrkhan618@gmail.com>
2025-08-13 12:25:59 +0530

    chore(deploy): no-op change in server.js to retrigger deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-// deploy: no-op marker for redeploy
+// deploy: no-op trigger for redeploy
 
 // Import Firebase service
 const firestoreService = require('./firebase-config');
