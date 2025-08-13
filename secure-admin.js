b46addfffd699761fdb65434251521c4f16ac7a4
Khan Automobiles <mrkhan618@gmail.com>
2025-08-13 15:29:10 +0530

    chore: bump deploy no-op marker in secure-admin.js

--- secure-admin.js
+++ secure-admin.js
@@ -1,6 +1,6 @@
 // Clean Secure Admin JavaScript - Works with simple server
 // No JWT tokens, no complex authentication - just working functionality
-// deploy: no-op 4
+// deploy: no-op 5
 
 // Global variables
 let currentUser = null;
